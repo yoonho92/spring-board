4185141b4282119b3fcd258a3eb8d2e70f8b19e0
yoonho <yoonho2015@gmail.com>
2022-01-24 23:41:51 +0900

    세션 오브젝트에서 state의 기본값을 설정하다

--- src/main/java/test/board/dto/UserStatusForSession.java
+++ src/main/java/test/board/dto/UserStatusForSession.java
@@ -2,13 +2,15 @@ package test.board.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserStatusForSession {
     private Long accountId;
     private String name;
-    private State state;
+    private State state = State.NONE;
 
     public enum State{
         SIGNING,
