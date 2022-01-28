62b1158236a48d68cb58b2767486c6de246d63be
yoonho <yoonho2015@gmail.com>
2022-01-28 18:44:25 +0900

    코멘트의 Res 모델을 추가, 수정하다. - post와 comment의 1:N  연관관계를 제거하고 comment의 목록 제공을 위한 Res모델을 추가, 수정하다.

--- src/main/java/test/board/dto/comment/SimpleSubComment.java
+++ src/main/java/test/board/dto/comment/SimpleSubComment.java
@@ -4,18 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class SimpleComment {
+public class SimpleSubComment {
     private Long id = -1L;
     private String content = "";
     private String author = "";
     private boolean isOwner = false;
-    private boolean isDelete = false;
-    private List<SimpleSubComment> simpleSubComments = new ArrayList<>();
+    private Long parentId = -1L;
 
 }
